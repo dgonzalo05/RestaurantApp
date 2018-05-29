c20a2daebdd91af7a5f34bc748fd45dd2810ae97
Yuhiro <powerofyuhiro@gmail.com>
2018-05-29 16:04:51 +0200

    mostrando lista

--- main/java/com/example/dgonzalo/restaurantapp/CartaActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/CartaActivity.java
@@ -3,13 +3,16 @@ package com.example.dgonzalo.restaurantapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.regex.Pattern;
 import java.util.ArrayList;
@@ -18,11 +21,9 @@ public class CartaActivity extends AppCompatActivity {
     private Button searchButton;
     private ImageButton homeButton;
     private TextView searchInput;
-    private ListView items;
-    /*private String[] carta = {"Ensalada","Macarrones","Paella","Chuleton","Pizza","Pescado al horno","Yogur","Helado","Trufas","Vino","Cola","Cerveza"};
-    private ArrayList<String> listItems = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
-    */
+    private ListView listView;
+    private String[] values = {"Ensalada","Macarrones","Paella","Chuleton","Pizza","Pescado al horno","Yogur","Helado","Trufas","Vino","Cola","Cerveza"};
+
 
 
     @Override
@@ -30,48 +31,68 @@ public class CartaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.carta_activity);
 
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        /*String[] values = new String[] { "Ensalada",
+                "Macarrones",
+                "Paella",
+                "Chuleton",
+                "Pizza",
+                "Pescado al horno",
+                "Yogur",
+                "Helado",
+                "Trufas",
+                "Vino",
+                "Cola",
+                "Cerveza"
+        };*/
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
 
-        /*searchInput = (TextView) findViewById(R.id.search_input);
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
-        items.setAdapter(adapter);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
 
-        items = (ListView)  findViewById(R.id.items);
-        items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            }
-        });
-
-        searchButton = (Button) findViewById(R.id.search_button);
-        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                listItems = findItems(searchInput.getText().toString());
-                adapter.notifyDataSetChanged();
-            }
-        });
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
 
-        homeButton = (ImageButton) findViewById(R.id.home_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CartaActivity.this ,MainActivity.class);
-                startActivity(intent);
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
             }
-        });*/
+
+        });
     }
 
-    /*public ArrayList<String> findItems (String text){
+    public ArrayList<String> findItems (String text){
         ArrayList<String> arr = new ArrayList<String>();
         Pattern pattern = Pattern.compile(text);
-        for (String s: carta) {
+        for (String s: values) {
             if (pattern.matcher(s).matches()) {
                 arr.add(s);
             }
         }
         return arr;
-    }*/
+    }
 }
