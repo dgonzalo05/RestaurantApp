8f92932d28af48a1cfd22c8c961274ba90bdd0d4
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-05-29 16:34:58 +0200

    comanda

--- main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
@@ -4,26 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-
-import java.util.ArrayList;
 
 public class ComandaActivity extends AppCompatActivity {
     private ImageButton homeButton;
-    private ArrayList<String> list_items = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ComandaActivity self = this;
-        homeButton = (ImageButton)findViewById(R.id.home_button);
-
+        setContentView(R.layout.comanda_activity);
+        homeButton = (ImageButton) findViewById(R.id.home_button);
         homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent intent = new Intent(self, MainActivity.class);
-                startActivity(intent);
+                changeIntent(CartaActivity.class);
             }
         });
-    }
+         }
+        public void changeIntent(Class c){
+            Intent intent = new Intent(this, c);
+            startActivity(intent);
+        }
 }
+
