877d3ecb09609aae347b54d73bbebe4c40763365
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-05-31 16:07:33 +0200

    comanda nova

--- main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
@@ -1,29 +1,43 @@
 package com.example.dgonzalo.restaurantapp;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ComandaActivity extends AppCompatActivity {
-    private ImageButton cartaButton;
+    private Button buyButton;
+    private Button deleteButton;
 
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.comanda_activity);
-        cartaButton = (ImageButton)findViewById(R.id.carta_button);
-        cartaButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.carta_activity);
+
+        buyButton = (Button) findViewById(R.id.buy_button);
+        buyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeIntent(CartaActivity.class);
+                Toast.makeText(ComandaActivity.this,R.string.buy_toast, Toast.LENGTH_SHORT).show();
+                changeIntent(MainActivity.class);
+            }
+
+        });
+        deleteButton = (Button) findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ComandaActivity.this,R.string.delete_toast, Toast.LENGTH_SHORT).show();
+                changeIntent(MainActivity.class);
             }
         });
-         }
-        public void changeIntent(Class c){
-            Intent intent = new Intent(this, c);
-            startActivity(intent);
-        }
-}
+    }
 
+    public void changeIntent(Class c){
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+    }
+}
