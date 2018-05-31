49b2bc30081c455dd3d171ed9d3cdbbb87e4b20f
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-05-31 15:02:15 +0200

    comanda button deleted from main

--- main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
@@ -1,19 +1,20 @@
 package com.example.dgonzalo.restaurantapp;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class ComandaActivity extends AppCompatActivity {
-    private ImageButton homeButton;
+    private ImageButton cartaButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.comanda_activity);
-        homeButton = (ImageButton) findViewById(R.id.home_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        cartaButton = (ImageButton)findViewById(R.id.carta_button);
+        cartaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeIntent(CartaActivity.class);
--- main/java/com/example/dgonzalo/restaurantapp/MainActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/MainActivity.java
@@ -8,29 +8,18 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button cartaButton;
-    private Button comandaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cartaButton = (Button)findViewById(R.id.carta_button);
-        comandaButton = (Button)findViewById(R.id.comanda_button);
-
-        comandaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeIntent(ComandaActivity.class);
-            }
-        });
-
         cartaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeIntent(CartaActivity.class);
             }
         });
-
     }
     public void changeIntent(Class c){
         Intent intent = new Intent(this, c);
