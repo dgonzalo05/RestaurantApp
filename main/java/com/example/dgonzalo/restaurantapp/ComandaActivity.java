4d0f94b432895d45697537d84cccea6a3d3c4ceb
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-05-31 16:43:06 +0200

    comanda

--- main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
+++ main/java/com/example/dgonzalo/restaurantapp/ComandaActivity.java
@@ -4,25 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ComandaActivity extends AppCompatActivity {
     private Button buyButton;
     private Button deleteButton;
-
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.comanda_activity);
+        listView = (ListView) findViewById(R.id.list);
+
+        Intent intent = getIntent();
+        String[] values = intent.getStringArrayExtra("carta");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
 
         buyButton = (Button) findViewById(R.id.buy_button);
         buyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ComandaActivity.this,R.string.buy_toast, Toast.LENGTH_SHORT).show();
-                //changeIntent(MainActivity.class);
+                changeIntent(MainActivity.class);
             }
 
         });
@@ -31,7 +41,7 @@ public class ComandaActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ComandaActivity.this,R.string.delete_toast, Toast.LENGTH_SHORT).show();
-                //changeIntent(MainActivity.class);
+                changeIntent(MainActivity.class);
             }
         });
     }
